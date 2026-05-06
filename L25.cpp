e7ebaaa0cd07ab881085258a4cd05a2638c8f0dc
Divyansh0002-codes <divyanshsaini0002@gmail.com>
2026-05-06 21:28:30 +0530

    Add C++ program to print triangle of letters

---
+++ L25.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int rows = 5;
+
+   
+    for (int i = 0; i < rows; ++i) {
+        
+       
+        for (int j = 0; j <= i; ++j) {
+            char ch = 'A' + j;
+            std::cout << ch;
+        }
+        
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
