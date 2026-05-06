83a0b03a69b63a49358c0c5b746bb778e2ad61b7
Divyansh0002-codes <divyanshsaini0002@gmail.com>
2026-05-06 21:20:42 +0530

    Create L20.cpp

---
+++ L20.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int rows = 3;
+    int cols = 5;
+
+ 
+    for (int i = 1; i <= rows; ++i) {
+        
+         for (int j = 1; j <= cols; ++j) {
+            std::cout << j << " ";
+        }
+        
+ 
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
