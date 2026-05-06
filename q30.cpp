06580383cf05f4ff9a52de709fc6f5efd25c105e
Divyansh0002-codes <divyanshsaini0002@gmail.com>
2026-05-06 19:50:58 +0530

    Create q30.cpp

---
+++ q30.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10];
+    int total=0,avg;
+    for(int i=1;i<=10;i++)
+    {
+        cin>>arr[i];
+        total = total + arr[i];
+        avg = total/10;
+    }
+    cout<<"TOTAL IS ="<<total;
+    cout<<"AVERAGE IS="<<avg;   
+}
